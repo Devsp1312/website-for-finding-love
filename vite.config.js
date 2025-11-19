747db58d7f704431c48f78c95977c5146fc5db64
Dev <devpatel132@yahoo.com>
2025-11-18 19:43:53 -0500

    chore: publish production site

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-// Set the `base` to the repository name so GitHub Pages (project page)
-// serves built assets from the correct path.
-export default defineConfig({
-  base: '/website-for-finding-love/'
-})
